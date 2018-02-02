9ac02a2c4c3e5e72aed1b476cde56c6a3f09a41e
Renwick James Hudspith <renwick.james.hudspith@gmail.com>
2018-02-02 17:50:45 -0500

    Updated blackbox and pade_laplace

--- src/EFFMASS/blackbox.c
+++ src/EFFMASS/blackbox.c
@@ -10,88 +10,46 @@
 #include <gsl/gsl_permutation.h>
 #include <gsl/gsl_complex.h>
 #include <gsl/gsl_complex_math.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_eigen.h>
 
 #include "gens.h"
 
 // standard Prony's method
 static void
-blackbox2( double complex *x ,
+blackbox1( double complex *x ,
 	   const double *data ,
 	   const size_t Ndata ,
 	   const size_t Nstates ,
 	   const size_t t ,
 	   const size_t Nt )
 {
-  size_t i , j , k , n ;
-  //int signum ;
-  double *Y = malloc( Nt*sizeof( double ) ) ;
-  double Y2[ Nt ][ Nstates ] ;
-  for( i = 0 ; i < Nt ; i++ ) {
-    const size_t yidx = ( t + Nstates + i )%Ndata ;
-    
-    for( j = 0 ; j < Nstates ; j++ ) {
-      const size_t didx = ( t + i + j )%Ndata ;
-      Y2[ i ][ j ] = -data[ didx ] ;
-    }
-    Y[i] = data[ yidx ] ;
-  }
+  size_t i , j ;
 
-  // make it into a square Nstates*Nstates matrix by left multiplying by Y2^T
   // gsl matrix allocations
-  gsl_matrix *alpha = gsl_matrix_alloc( Nstates , Nstates ) ;
+  gsl_matrix *alpha = gsl_matrix_alloc( Nt , Nstates ) ;
   gsl_matrix *Q     = gsl_matrix_alloc( Nstates , Nstates ) ;
   gsl_vector *S     = gsl_vector_alloc( Nstates ) ;
   gsl_vector *Work  = gsl_vector_alloc( Nstates ) ;
   
-  gsl_vector *beta  = gsl_vector_alloc( Nstates ) ;
+  gsl_vector *beta  = gsl_vector_alloc( Nt ) ;
   gsl_vector *delta = gsl_vector_alloc( Nstates ) ;
+
   
-  for( i = 0 ; i < Nstates ; i++ ) {
-    register double sum = 0.0 ;
+  for( i = 0 ; i < Nt ; i++ ) {
     for( j = 0 ; j < Nstates ; j++ ) {
-      sum = 0.0 ;
-      for( k = 0 ; k < Nt ; k++ ) {
-	sum += Y2[ k ][ i ] * Y2[ k ][ j ] ;
-      }
-      gsl_matrix_set( alpha , i , j , sum ) ;
+      const size_t didx = ( t + i + j )%Ndata ;
+      gsl_matrix_set( alpha , i , j , -data[ didx ] ) ;
     }
-    sum = 0.0 ;
-    for( k = 0 ; k < Nt ; k++ ) {
-      sum += Y2[ k ][ i ] * Y[k] ;
-    }
-    gsl_vector_set( beta , i , sum ) ;
   }
 
-#if 0
-  printf( "%f \n" , data[t] ) ;
-  printf( "%f \n" , data[(t+1)%Ndata] ) ;
-
-  // have a look at it
-  printf( "Ymatrix\n" ) ;
-  for( i = 0 ; i < Nstates ; i++ ) {
-    for( j = 0 ; j < Nstates ; j++ ) {
-      printf( " %f " , gsl_matrix_get( alpha , i , j ) ) ;
-    }
-    printf( "\n" ) ;
+  for( i = 0 ; i < Nt ; i++ ) {
+    const size_t yidx = ( t + Nstates + i )%Ndata ;
+    gsl_vector_set( beta , i , data[ yidx ] ) ;
   }
   
-  printf( "Ys\n" ) ;
-  for( i = 0 ; i < Nstates ; i++ ) {
-    printf( " %f " , gsl_vector_get( beta , i ) ) ;
-  }
-  printf( "\n" ) ;
-#endif
-
   gsl_linalg_SV_decomp( alpha , Q , S , Work ) ;
 
-  for( i = 0 ; i < Nstates ; i++ ) {
-    //printf( "Singular values %zu %e \n" , i , gsl_vector_get( S , i ) ) ;
-    if( gsl_vector_get( S , i ) < gsl_vector_get( S , 0 )*1E-6 ) {
-      gsl_vector_set( S , i , 0.0 ) ;
-    }
-  }
-  //exit(1) ;
-  
   gsl_linalg_SV_solve( alpha , Q , S , beta , delta ) ;
 
   //printf( "States \n" ) ;
@@ -99,7 +57,6 @@ blackbox2( double complex *x ,
 
   for( i = 0 ; i < Nstates ; i++ ) {
     coeffs[ i ] = gsl_vector_get( delta , i ) ;
-    //printf( "%f\n" , gsl_vector_get( delta , i ) ) ;
   }
   coeffs[Nstates] = 1 ;
 
@@ -110,25 +67,266 @@ blackbox2( double complex *x ,
   gsl_poly_complex_solve( coeffs , Nstates+1 , w , z ) ;
   
   //
-  //printf( "SOLS\n" ) ;
   for( i = 0 ; i < Nstates ; i++ ) {
-    x[ i ] = z[ 2*i ] + I * z[ 2*i+1 ] ;
-    //printf( "%f %f \n" , creal( x[i] ) , cimag( x[i] ) ) ;
+    x[ i ] = ( z[ 2*i ] + I * z[ 2*i+1 ] ) ;
   }
-      
-  // free temporary Y-data
-  free( Y ) ;
-  free( z ) ;
 
   gsl_poly_complex_workspace_free( w ) ;
 
   // free the gsl vectors
   gsl_matrix_free( alpha ) ;
   gsl_matrix_free( Q ) ;
+  gsl_vector_free( S ) ;
+  gsl_vector_free( Work ) ;
   
   gsl_vector_free( beta ) ;
   gsl_vector_free( delta ) ;
+
+  return ;
+}
+
+// standard Prony's method
+static void
+blackbox1p5( double complex *x ,
+	     const double *data ,
+	     const size_t Ndata ,
+	     const size_t Nstates ,
+	     const size_t t ,
+	     const size_t Nt )
+{
+  size_t i , j ;
+
+  // gsl matrix allocations
+  gsl_matrix *alpha = gsl_matrix_alloc( Nt , Nt ) ;
+  gsl_matrix *Q     = gsl_matrix_alloc( Nt , Nt ) ;
+  gsl_vector *S     = gsl_vector_alloc( Nt ) ;
+  gsl_vector *Work  = gsl_vector_alloc( Nt ) ;
+  
+  gsl_vector *beta  = gsl_vector_alloc( Nt ) ;
+  gsl_vector *delta = gsl_vector_alloc( Nt ) ;
+
+  
+  for( i = 0 ; i < Nt ; i++ ) {
+    for( j = 0 ; j < Nt ; j++ ) {
+      const size_t didx = ( t + i + j )%Ndata ;
+      gsl_matrix_set( alpha , i , j , -data[ didx ] ) ;
+    }
+  }
+
+  for( i = 0 ; i < Nt ; i++ ) {
+    const size_t yidx = ( t + Nt + i )%Ndata ;
+    gsl_vector_set( beta , i , data[ yidx ] ) ;
+  }
+  
+  gsl_linalg_SV_decomp( alpha , Q , S , Work ) ;
+
+  for( i = Nstates ; i < Nt ; i++ ) {
+    gsl_vector_set( S , i , 0.0 ) ;
+  }
+  
+  gsl_linalg_SV_solve( alpha , Q , S , beta , delta ) ;
+
+  //printf( "States \n" ) ;
+  double coeffs[ Nt+1 ] ;
+
+  for( i = 0 ; i < Nt ; i++ ) {
+    coeffs[ i ] = gsl_vector_get( delta , i ) ;
+  }
+  coeffs[Nt] = 1 ;
+
+  gsl_poly_complex_workspace *w =
+    gsl_poly_complex_workspace_alloc( Nt+1 ) ;
+  double *z = malloc( 2 * Nt * sizeof( double ) ) ;
+  
+  gsl_poly_complex_solve( coeffs , Nt+1 , w , z ) ;
+  
+  //
+  for( i = 0 ; i < Nstates ; i++ ) {
+    x[ i ] = ( z[ 2*(Nt-Nstates+i) ] + I * z[ 2*(Nt-Nstates+i)+1 ] ) ;
+    //printf( "%zu %e \n" , i , creal( -clog( z[ 2*i ] ) ) ) ;
+  }
+
+  gsl_poly_complex_workspace_free( w ) ;
+
+  // free the gsl vectors
+  gsl_matrix_free( alpha ) ;
+  gsl_matrix_free( Q ) ;
+  gsl_vector_free( S ) ;
+  gsl_vector_free( Work ) ;
     
+  gsl_vector_free( beta ) ;
+  gsl_vector_free( delta ) ;
+
+  return ;
+}
+
+// TLS variant
+static void
+blackbox3( double complex *x ,
+	   const double *data ,
+	   const size_t Ndata ,
+	   const size_t Nstates ,
+	   const size_t t ,
+	   size_t Nt )
+{
+  size_t i , j ;
+
+  if( Nt <= Nstates ) Nt += 1 ;
+
+  // gsl matrix allocations
+  gsl_matrix *alpha = gsl_matrix_alloc( Nt , Nstates+1 ) ;
+  gsl_matrix *Q     = gsl_matrix_alloc( Nstates+1 , Nstates+1 ) ;
+  gsl_vector *S     = gsl_vector_alloc( Nstates+1 ) ;
+  gsl_vector *Work  = gsl_vector_alloc( Nstates+1 ) ;
+
+  for( i = 0 ; i < Nt ; i++ ) {
+    for( j = 0 ; j < Nstates ; j++ ) {
+      const size_t didx = ( t + i + j )%Ndata ;
+      gsl_matrix_set( alpha , i , j , -data[ didx ] ) ;
+    }
+    const size_t yidx = ( t + Nstates + i )%Ndata ;
+    gsl_matrix_set( alpha , i , j , data[ yidx ] ) ;
+  }
+    
+  gsl_linalg_SV_decomp( alpha , Q , S , Work ) ;
+
+  const double NORM = -1.0 / gsl_matrix_get( Q , Nstates , Nstates ) ;
+  if( fabs( NORM ) < 1E-15 ) {
+    printf( "Singular NORM %e \n" , NORM ) ;
+  }
+  
+  double coeffs[ Nstates+1 ] ;
+  for( i = 0 ; i < Nstates ; i++ ) {
+    coeffs[ i ] = NORM * gsl_matrix_get( Q , i , Nstates ) ;
+  }
+  coeffs[Nstates] = 1 ;
+  
+  // free the gsl vectors and matrices
+  gsl_matrix_free( alpha ) ;
+  gsl_matrix_free( Q ) ;
+  gsl_vector_free( S ) ;
+  gsl_vector_free( Work ) ;
+  
+  gsl_poly_complex_workspace *w =
+    gsl_poly_complex_workspace_alloc( Nstates+1 ) ;
+  double *z = malloc( 2 * Nstates * sizeof( double ) ) ;
+  
+  if( gsl_poly_complex_solve( coeffs , Nstates+1 , w , z ) != GSL_SUCCESS ) {
+    return ;
+  }
+  
+  //
+  for( i = 0 ; i < Nstates ; i++ ) {
+    x[ i ] = z[ 2*i ] + I * z[ 2*i+1 ] ;
+  }
+
+  gsl_poly_complex_workspace_free( w ) ;
+
+  return ;
+}
+
+// HTLS variant
+static void
+blackbox4( double complex *x ,
+	   const double *data ,
+	   const size_t Ndata ,
+	   const size_t Nstates ,
+	   const size_t t ,
+	   const size_t Nt )
+{
+  size_t i , j ;
+
+  // gsl matrix allocations
+  gsl_matrix *alpha = gsl_matrix_alloc( Nt , Nstates+1 ) ;
+  gsl_matrix *Q     = gsl_matrix_alloc( Nstates+1 , Nstates+1 ) ;
+  gsl_vector *S     = gsl_vector_alloc( Nstates+1 ) ;
+  gsl_vector *Work  = gsl_vector_alloc( Nstates+1 ) ;
+
+  for( i = 0 ; i < Nt ; i++ ) {
+    for( j = 0 ; j < Nstates ; j++ ) {
+      const size_t didx = ( t + i + j )%Ndata ;
+      gsl_matrix_set( alpha , i , j , -data[ didx ] ) ;
+    }
+    const size_t yidx = ( t + Nstates + i )%Ndata ;
+    gsl_matrix_set( alpha , i , j , data[ yidx ] ) ;
+  }
+
+  gsl_linalg_SV_decomp( alpha , Q , S , Work ) ;
+  gsl_matrix_free( Q ) ;
+  gsl_vector_free( Work ) ;
+
+  // truncate Hankel matrix to rank K, i.e. Nstates
+  gsl_matrix *HK = gsl_matrix_alloc( 2*Nstates , Nstates ) ;
+  for( i = 0 ; i < Nstates ; i++ ) {
+    for( j = 0 ; j < Nstates ; j++ ) {
+      gsl_matrix_set( HK , i , j ,
+		      gsl_matrix_get( alpha , i , j ) ) ;
+      gsl_matrix_set( HK , Nstates + i , j ,
+		      gsl_matrix_get( alpha , i + 1 , j ) ) ;
+    }
+  }
+
+  // free the gsl vectors
+  gsl_matrix_free( alpha ) ;
+
+  gsl_matrix *Qq     = gsl_matrix_alloc( Nstates , Nstates ) ;
+  gsl_vector *Ss     = gsl_vector_alloc( Nstates ) ;
+  gsl_vector *WorkQ  = gsl_vector_alloc( Nstates ) ;
+
+  gsl_linalg_SV_decomp( HK , Qq , Ss , WorkQ ) ;
+
+  gsl_matrix_free( Qq ) ;
+  gsl_vector_free( Ss ) ;
+  gsl_vector_free( WorkQ ) ;
+
+  // split HK into two
+  gsl_matrix *A = gsl_matrix_alloc( Nstates , Nstates ) ;
+  gsl_matrix *B = gsl_matrix_alloc( Nstates , Nstates ) ;
+  gsl_matrix *Binv = gsl_matrix_alloc( Nstates , Nstates ) ;
+  gsl_matrix *C = gsl_matrix_alloc( Nstates , Nstates ) ;
+  
+  for( i = 0 ; i < Nstates ; i++ ) {
+    for( j = 0 ; j < Nstates ; j++ ) {
+      gsl_matrix_set( A , i , j , gsl_matrix_get( HK , i , j ) ) ;
+      gsl_matrix_set( B , i , j , gsl_matrix_get( HK , i + Nstates , j  ) ) ;
+    }
+  }
+
+  // invert B and multiply by A
+  int signum ;
+  gsl_permutation *perm = gsl_permutation_alloc( Nstates ) ;
+  
+  gsl_linalg_LU_decomp( B , perm , &signum ) ;
+  gsl_linalg_LU_invert( B , perm , Binv ) ;
+
+  for( i = 0 ; i < Nstates ; i++ ) {
+    for( j = 0 ; j < Nstates ; j++ ) {
+      register double loc_sum = 0.0 ;
+      size_t k ;
+      for( k = 0 ; k < Nstates ; k++ ) {
+	loc_sum +=
+	  gsl_matrix_get( A , i , k ) * gsl_matrix_get( Binv , k , j ) ;
+      }
+      gsl_matrix_set( C , i , j , loc_sum ) ;
+    }
+  }
+
+  // compute the eigenvalues of C
+  gsl_vector_complex *eval = gsl_vector_complex_alloc( Nstates ) ;
+  gsl_eigen_nonsymm_workspace *w = gsl_eigen_nonsymm_alloc( Nstates ) ;
+  gsl_eigen_nonsymm( C , eval , w ) ;
+
+  for( i = 0 ; i < Nstates ; i++ ) {
+    x[i] = GSL_REAL( gsl_vector_complex_get( eval , i ) ) + I * GSL_IMAG( gsl_vector_complex_get( eval , i ) ) ;
+  }
+
+  gsl_matrix_free( A ) ;
+  gsl_matrix_free( B ) ;
+  gsl_matrix_free( Binv  ) ;
+  gsl_matrix_free( C  ) ;
+  gsl_vector_complex_free( eval ) ;
+  gsl_eigen_nonsymm_free( w ) ;
+
   return ;
 }
 
@@ -144,6 +342,93 @@ comp( const void *elem1 ,
   return 0 ;
 }
 
+// qsort comparison
+static int 
+comp_sort( const void *elem1 , 
+	   const void *elem2 ) 
+{
+  const double complex f = *( (double complex*)elem1 ) ;
+  const double complex s = *( (double complex*)elem2 ) ;
+  if( fabs(cimag(f)) > fabs(cimag(s)) ) return  1 ;
+  if( fabs(cimag(f)) < fabs(cimag(s)) ) return -1 ;
+  return 0 ;
+}
+
+/*
+  So the data is very badly behaved, always
+  what we want to do is have some data we trust and 
+  some that we are unsure of
+
+  worst offenders ( Nbad ) : solutions which are negative
+                             or will yield a negative mass
+
+  second worst ( Niffy ) : ones with imaginary parts
+  
+  we sort the real solutions into the trust list
+
+  then we sort the imaginary ones
+
+  and finally the worst ones
+ */
+static void
+get_safe_masses( const size_t NSTATES ,
+		 const size_t NDATA ,
+		 double masses[ NSTATES ][ NDATA ] ,
+		 const double complex *x ,
+		 const size_t t )
+{
+  size_t Ntrust = 0 , Niffy = 0 , Nbad = 0 , i ;
+  for( i = 0 ; i < NSTATES ; i++ ) {
+    printf( "PRON_%zu %zu  %e %e \n" , i , t , creal( x[i] ) , cimag( x[i] ) ) ;
+    if( creal( x[i] ) < 1.03 ) {
+      Nbad++ ;
+    } else if( fabs( cimag( x[i] ) ) < 1E-12 ) {
+      Ntrust++ ;
+    }
+  }
+  Niffy = NSTATES - Ntrust - Nbad ;
+
+  printf( "Ntrust %zu , Niffy %zu , Nbad %zu \n" , Ntrust , Niffy , Nbad ) ;
+
+  size_t t_idx = 0 , i_idx = 0 , b_idx = 0 ;
+  double etrust[ Ntrust ] , eiffy[ Niffy ] , ebad[ Nbad ] ;
+  for( i = 0 ; i < NSTATES ; i++ ) {
+    if( creal( x[i] ) < 1.03 ) {
+      ebad[ b_idx ] = fabs( creal( clog( x[i] ) ) ) ; b_idx++ ;
+    } else if( fabs( cimag( x[i] ) ) < 1E-12 ) {
+      etrust[ t_idx ] = fabs( creal( clog( x[i] ) ) ) ; t_idx++ ;
+    } else {
+      eiffy[ i_idx ] = fabs( creal( clog( x[i] ) ) ) ; i_idx++ ;
+    }
+  }
+  printf( "Sols did \n" ) ;
+
+  // sort them
+  qsort( etrust , Ntrust , sizeof(double) , comp ) ;
+  qsort( eiffy , Niffy, sizeof(double) , comp ) ;
+  qsort( ebad , Nbad , sizeof(double) , comp ) ;
+
+  size_t idx = 0 ;
+  for( i = 0 ; i < Ntrust ; i++ ) {
+    masses[idx][t] = etrust[i] ;
+    idx++ ;
+  }
+  for( i = 0 ; i < Niffy ; i++ ) {
+    masses[idx][t] = eiffy[i] ;
+    idx++ ;
+  }
+  for( i = 0 ; i < Nbad ; i++ ) {
+    masses[idx][t] = ebad[i] ;
+    idx++ ;
+  }
+
+  for( i = 0 ; i < NSTATES ; i++ ) {
+    printf( "Masses_%zu %e \n" , i , masses[i][t] ) ;
+  }
+  
+  return ;
+}
+
 // compute the blackbox effective mass for a correlator of y-data
 void
 blackbox( const double *data ,
@@ -155,28 +440,14 @@ blackbox( const double *data ,
   const size_t smallt = NDATA ;
 
   double complex *x = malloc( NSTATES * sizeof( double complex ) ) ;
-  double *e = malloc( NSTATES * sizeof( double ) ) ;
-  
-  size_t t , i , j ;
+
+  size_t t ;
   for( t = 0 ; t < smallt ; t++ ) {
 
-    blackbox2( x , data , NDATA , NSTATES , t , NSTATES ) ;
+    blackbox1( x , data , NDATA , NSTATES , t , NSTATES+10 ) ;
     
-    // loggy log log .... x[i] = exp( -e[i] )
-    for( i = 0 ; i < NSTATES ; i++ ) {
-      e[ i ] = fabs( creal( clog( x[i] ) ) ) ;
-      //printf( "%f %f -> %f\n" , creal( x[i] ) , cimag( x[i] ) , e[i] ) ;
-    }
-    
-    // sort the e's ?
-    qsort( e , NSTATES , sizeof(double) , comp ) ;
-	
-    for( i = 0 ; i < NSTATES ; i++ ) {
-      masses[i][t] = e[i] ;
-    }
+    get_safe_masses( NSTATES , NDATA , masses , x , t ) ;
   }
-
-  free( e ) ;
   free( x ) ;
 
   return ;
--- src/UTILS/pade_laplace.c
+++ src/UTILS/pade_laplace.c
@@ -11,10 +11,34 @@
 
 #include "pade_coefficients.h"
 
+//#define VERBOSE
+
+// LUT for the factorial
+static const double fac[169] = { 1.000000e+00 , 1.000000e+00 , 2.000000e+00 , 6.000000e+00 , 2.400000e+01 , 1.200000e+02 , 7.200000e+02 , 5.040000e+03 , 4.032000e+04 , 
+3.628800e+05 , 3.628800e+06 , 3.991680e+07 , 4.790016e+08 , 6.227021e+09 , 8.717829e+10 , 1.307674e+12 , 2.092279e+13 , 
+3.556874e+14 , 6.402374e+15 , 1.216451e+17 , 2.432902e+18 , 5.109094e+19 , 1.124001e+21 , 2.585202e+22 , 6.204484e+23 , 
+1.551121e+25 , 4.032915e+26 , 1.088887e+28 , 3.048883e+29 , 8.841762e+30 , 2.652529e+32 , 8.222839e+33 , 2.631308e+35 , 
+8.683318e+36 , 2.952328e+38 , 1.033315e+40 , 3.719933e+41 , 1.376375e+43 , 5.230226e+44 , 2.039788e+46 , 8.159153e+47 , 
+3.345253e+49 , 1.405006e+51 , 6.041526e+52 , 2.658272e+54 , 1.196222e+56 , 5.502622e+57 , 2.586232e+59 , 1.241392e+61 , 
+6.082819e+62 , 3.041409e+64 , 1.551119e+66 , 8.065818e+67 , 4.274883e+69 , 2.308437e+71 , 1.269640e+73 , 7.109986e+74 , 
+4.052692e+76 , 2.350561e+78 , 1.386831e+80 , 8.320987e+81 , 5.075802e+83 , 3.146997e+85 , 1.982608e+87 , 1.268869e+89 , 
+8.247651e+90 , 5.443449e+92 , 3.647111e+94 , 2.480036e+96 , 1.711225e+98 , 1.197857e+100 , 8.504786e+101 , 6.123446e+103 , 
+4.470115e+105 , 3.307885e+107 , 2.480914e+109 , 1.885495e+111 , 1.451831e+113 , 1.132428e+115 , 8.946182e+116 , 7.156946e+118 , 
+5.797126e+120 , 4.753643e+122 , 3.945524e+124 , 3.314240e+126 , 2.817104e+128 , 2.422710e+130 , 2.107757e+132 , 1.854826e+134 , 
+1.650796e+136 , 1.485716e+138 , 1.352002e+140 , 1.243841e+142 , 1.156773e+144 , 1.087366e+146 , 1.032998e+148 , 9.916779e+149 , 
+9.619276e+151 , 9.426890e+153 , 9.332622e+155 , 9.332622e+157 , 9.425948e+159 , 9.614467e+161 , 9.902901e+163 , 1.029902e+166 , 
+1.081397e+168 , 1.146281e+170 , 1.226520e+172 , 1.324642e+174 , 1.443860e+176 , 1.588246e+178 , 1.762953e+180 , 1.974507e+182 , 
+2.231193e+184 , 2.543560e+186 , 2.925094e+188 , 3.393109e+190 , 3.969937e+192 , 4.684526e+194 , 5.574586e+196 , 6.689503e+198 , 
+8.094299e+200 , 9.875044e+202 , 1.214630e+205 , 1.506142e+207 , 1.882677e+209 , 2.372173e+211 , 3.012660e+213 , 3.856205e+215 , 
+4.974504e+217 , 6.466855e+219 , 8.471581e+221 , 1.118249e+224 , 1.487271e+226 , 1.992943e+228 , 2.690473e+230 , 3.659043e+232 , 
+5.012889e+234 , 6.917786e+236 , 9.615723e+238 , 1.346201e+241 , 1.898144e+243 , 2.695364e+245 , 3.854371e+247 , 5.550294e+249 , 
+8.047926e+251 , 1.174997e+254 , 1.727246e+256 , 2.556324e+258 , 3.808923e+260 , 5.713384e+262 , 8.627210e+264 , 1.311336e+267 , 
+2.006344e+269 , 3.089770e+271 , 4.789143e+273 , 7.471063e+275 , 1.172957e+278 , 1.853272e+280 , 2.946702e+282 , 4.714724e+284 , 
+7.590705e+286 , 1.229694e+289 , 2.004402e+291 , 3.287219e+293 , 5.423911e+295 , 9.003692e+297 , 1.503617e+300 , 2.526076e+302 };
+
 // compute the coefficients
 static void
 dLdp( double *d ,
-      const double *fac ,
       const size_t Nders ,
       const double *x ,
       const double *y ,
@@ -34,7 +58,7 @@ dLdp( double *d ,
       epx[j] *= -x[j] ;
     }
     epx[j] *= -x[j] ;
-    d[i] /= ( 2 * fac[i] ) ;
+    d[i] /= ( 2. * fac[i] ) ;
     #ifdef VERBOSE
     printf( "D[ %zu ] %e %e \n" , i , d[i] , fac[i] ) ;
     #endif
@@ -49,96 +73,123 @@ comp( const void *elem1 ,
 {
   const double f = *( (double*)elem1 ) ;
   const double s = *( (double*)elem2 ) ;
-  if (f > s) return  1 ;
-  if (f < s) return -1 ;
+  if (f < s) return  1 ;
+  if (f > s) return -1 ;
   return 0 ;
 }
 
-static int
-stable_pade( double *sims ,
+typedef enum { GOOD , BAD , UGLY } GBU ;
+
+static GBU
+filter( const double complex z )
+{
+  if( creal( z ) > 0 ) {
+    return BAD ;
+  } else if( fabs( cimag( z ) ) < 1E-12 ) {
+    return GOOD ;
+  } else {
+    return UGLY ;
+  }
+}
+
+static size_t
+stable_pade( double *masses ,
 	     const double *d ,
 	     const size_t Nexp ,
 	     const double p0 )
 {
-  double results[ Nexp ][ Nexp ] ;
-  size_t i , j ;
-  for( j = 0 ; j < Nexp ; j++ ) {
-
-    sims[j] = 0.0 ;
-    
-    for( i = 0 ; i < Nexp ; i++ ) {
-      results[j][i] = 0.0 ;
-    }
-    
-    const size_t N = Nexp*Nexp + j ;
-    
-    double *pade = malloc( 2*N * sizeof( double ) ) ;
-    pades_from_poly( pade , d , N , N ) ;
-
-    double *denom = malloc( ( N + 1 ) * sizeof( double ) ) ;
-    denom[0] = 1 ;
-    for( i = 0 ; i < N  ; i++ ) {
-      denom[ i + 1 ] = pade[ N + i ] ;
-    }
-
-    gsl_poly_complex_workspace *w =		\
-      gsl_poly_complex_workspace_alloc( N+1 ) ;
-    double *z = malloc( 2 * ( N ) * sizeof( double ) ) ;
+  size_t i ;
   
-    gsl_poly_complex_solve( denom , N+1 , w , z ) ;
+  const size_t N = 4*Nexp ;
+  double *pade = malloc( 2*N * sizeof( double ) ) ;
+  
+  pades_from_poly( pade , d , N , N ) ;
+#ifdef VERBOSE
+  for( i = 0 ; i < 2*N ; i++ ) {
+    printf( "[PLAP] pade_%zu %e \n" , i , pade[i] ) ;
+  }
+#endif
 
-    // set the real data
-    size_t idx = 0 ;
-    for( i = 0 ; i < N ; i++ ) {
-      //#ifdef VERBOSE
-      printf( "RESULTS :: %zu %zu -> %f %f \n",
-	      j , i , z[2*i] + p0 , z[2*i+1] + p0) ;
-      //#endif
-      if( fabs( z[2*i+1] ) < 1E-6 && // is purely real
-	  fabs( z[2*i] + p0 ) < 5 && // is small
-	  ( z[2*i] + p0 ) < 0        // is less than zero
-	  ) {
-	if( idx < Nexp ) {
-	  results[j][idx] = z[2*i] + p0 ;
-	  #ifdef VERBOSE
-	  printf( "Results post filter : %f \n" , results[j][idx] ) ;
-	  #endif
-	}
-	idx++ ;
-      }
-    }
-    gsl_poly_complex_workspace_free( w ) ;
-    free( pade ) ;
-    free( denom ) ;
-    free( z ) ;
+  // use a root-finding algorithm to determine the poles
+  double *denom = malloc( ( N + 1 ) * sizeof( double ) ) ;
+  denom[ 0 ] = 1 ;
+  for( i = 0 ; i < N  ; i++ ) {
+    denom[ i + 1 ] = pade[ N + i ] ;
   }
 
-  // quicksort the data
-  int Flag = SUCCESS ;
-  for( i = 0 ; i < Nexp ; i++ ) {
-    qsort( results[i] , Nexp , sizeof(double) , comp ) ;
-  }
-  for( i = 0 ; i < Nexp ; i++ ) {
-    size_t Ntot = 0 ;
-    sims[i] = 0.0 ;
-    for( j = 0 ; j < Nexp ; j++ ) {
-      if( results[j][i] != 0.0 ) {
-	sims[i] += results[j][i] ;
-	Ntot++ ;
-      }
-    }
-    if( Ntot == 0 ) {
-      fprintf( stderr , "[PADE-LAPLACE] could not determine all of the pole"
-	       " masses Problem with -> Exp %zu \n" , i ) ;
-      Flag = FAILURE ;
-    } else {
-      sims[i] /= Ntot ;
+  gsl_poly_complex_workspace *w =		\
+    gsl_poly_complex_workspace_alloc( N+1 ) ;
+  double complex *z = malloc( N * sizeof( double complex ) ) ;
+  
+  gsl_poly_complex_solve( denom , N+1 , w , (double*)z ) ;
+
+  // check usual possible solutions
+  size_t Ngood = 0 , Nbad = 0 , Nugly = 0 ;
+  for( i = 0 ; i < N ; i++ ) {
+    z[i] += p0 ;
+    #ifdef VERBOSE
+    fprintf( stdout , "[PLAP] Z_%zu %f %f \n" ,
+	     i , creal( z[i] ) , cimag( z[i] ) ) ;
+    #endif
+    switch( filter( z[i] ) ) {
+    case GOOD : Ngood++ ; break ;
+    case BAD : Nbad++ ; break ;
+    case UGLY : Nugly++ ; break ;
     }
   }
-  return Flag ;
+  #ifdef VERBOSE
+  fprintf( stdout , "[PLAP] Ngood %zu, Nbad %zu , Nugly %zu \n" ,
+	   Ngood , Nbad , Nugly ) ;
+  #endif
+
+  size_t gidx = 0 , bidx = 0 , uidx = 0 ;
+  double Good[ Ngood ] , Bad[ Nbad ] , Ugly[ Nugly ] ;
+  for( i = 0 ; i < N ; i++ ) {
+    switch( filter( z[i] ) ) {
+    case GOOD : Good[ gidx++ ] = creal( z[i] ) ; break ;
+    case BAD : Bad[ bidx++ ] = creal( z[i] ) ; break ;
+    case UGLY : Ugly[ uidx++ ] = creal( z[i] ) ; break ;
+    }
+  }
+
+  qsort( Good , Ngood , sizeof(double) , comp ) ;
+  qsort( Ugly , Nugly , sizeof(double) , comp ) ;
+  qsort( Bad , Nbad , sizeof(double) , comp ) ;
+
+  size_t idx = 0 ;
+  for( i = 0 ; i < Ngood ; i++ ) {
+    if( idx >= Nexp ) goto end ;
+    masses[ idx++ ] = -Good[i] ;
+  }
+
+  for( i = 0 ; i < Nugly ; i++ ) {
+    if( idx >= Nexp ) goto end ;
+    masses[ idx++ ] = -Ugly[i] ;
+  }
+
+  for( i = 0 ; i < Nbad ; i++ ) {
+    if( idx >= Nexp ) goto end ;
+    masses[ idx++ ] = -Bad[i] ;
+  }
+
+
+end :
+
+  #ifdef VERBOSE
+  for( i = 0 ; i < Nexp ; i++ ) {
+    printf( "Mass_%zu %e \n" , i , masses[i] ) ;
+  }
+  #endif
+  
+  gsl_poly_complex_workspace_free( w ) ;
+  free( denom ) ;
+  free( pade ) ;
+  free( z ) ;
+
+  return Ngood ;
 }
 
-// get amplitudes
+// get amplitudes by SVD
 static int
 get_amplitudes( double *Amps ,
 		const double *x ,
@@ -147,59 +198,49 @@ get_amplitudes( double *Amps ,
 		const double *masses ,
 		const double Nexps )
 {
-  // compute the amplitudes using
-  // ( A^T A ) A^T y = x
-  // where the matrix A is e^mx
-  gsl_matrix *alpha = gsl_matrix_alloc( Nexps , Nexps ) ;
-  gsl_vector *beta  = gsl_vector_alloc( Nexps ) ;
+  gsl_matrix *alpha = gsl_matrix_alloc( Ndata-1 , Nexps ) ;
+  gsl_matrix *Q     = gsl_matrix_alloc( Nexps , Nexps ) ;
+  gsl_vector *S     = gsl_vector_alloc( Nexps ) ;
+  gsl_vector *Work  = gsl_vector_alloc( Nexps ) ;
+  
+  gsl_vector *beta  = gsl_vector_alloc( Ndata-1 ) ;
   gsl_vector *delta = gsl_vector_alloc( Nexps ) ;
-  gsl_permutation *perm = gsl_permutation_alloc( Nexps ) ;
 
-  size_t i , j , k ;
-  for( i = 0 ; i < Nexps ; i++ ) {
+  size_t i , j ;
+  for( i = 1 ; i < Ndata ; i++ ) {
     // set alpha
-    register double sum ;
-    for( j = i ; j < Nexps ; j++ ) {
-      sum = 0.0 ;
-      for( k = 0 ; k < Ndata ; k++ ) {
-	sum += exp( ( masses[i] + masses[j] ) * x[k] ) ;
-      }
-      gsl_matrix_set( alpha , i , j , sum ) ;
-      gsl_matrix_set( alpha , j , i , sum ) ;
+    for( j = 0 ; j < Nexps ; j++ ) {
+      gsl_matrix_set( alpha , i-1 , j , exp( -masses[j] * x[i] ) ) ;
     }
-    // set beta
-    sum = 0.0 ;
-    for( k = 0 ; k < Ndata ; k++ ) {
-      sum += y[k] * exp( masses[i] * x[k] ) ;
-    }
-    gsl_vector_set( beta , i , sum ) ;
+    gsl_vector_set( beta , i-1 , y[i] ) ;
   }
 
-  // solves alpha[p][q] * delta( a[q] ) = beta[p] for delta
-  int signum , Flag = SUCCESS ;
-  if( gsl_linalg_LU_decomp( alpha , perm , &signum ) != GSL_SUCCESS ) {
-    fprintf( stderr , "[POLY_COEFF] LU decomp failed, rolling back to SVD\n" ) ;
-    Flag = FAILURE ;
+  if( gsl_linalg_SV_decomp( alpha , Q , S , Work ) != GSL_SUCCESS ) {
+    return FAILURE ;
   }
-  if( gsl_linalg_LU_solve( alpha , perm , beta , delta ) != GSL_SUCCESS ) {
-    fprintf( stderr , "[POLY_COEFF] LU solve failure \n" ) ;
-    Flag = FAILURE ;
+  if( gsl_linalg_SV_solve( alpha , Q , S , beta , delta ) != GSL_SUCCESS ) {
+    return FAILURE ;
   }
 
   // tell us the result
   for( i = 0 ; i < Nexps ; i++ ) {
+    #ifdef VERBOSE
     fprintf( stdout , "%f e ^ { %f * x } \n" ,
 	     gsl_vector_get( delta , i ) , masses[i] ) ;
+    #endif
     Amps[i] = gsl_vector_get( delta , i ) ;
   }
 
   // free the gsl vectors
-  gsl_vector_free( beta ) ;
   gsl_matrix_free( alpha ) ;
-  gsl_permutation_free( perm ) ;
+  gsl_matrix_free( Q ) ;
+  gsl_vector_free( S ) ;
+  gsl_vector_free( Work ) ;
+  
+  gsl_vector_free( beta ) ;
   gsl_vector_free( delta ) ;
 
-  return Flag ;
+  return SUCCESS ;
 }
 
 int
@@ -207,27 +248,42 @@ pade_laplace( double *fparams ,
 	      const double *x ,
 	      const double *y ,
 	      const size_t Ndata ,
-	      const size_t Nexps )
+	      const size_t Nexps ,
+	      const double p0 )
 {
-  const size_t Nders = 30 ;
+  const size_t Nders = 4 * ( (Nexps + 8) ) + 1 ;
+  if( Nders > 169 ) {
+    fprintf( stderr , "[PLAP] too many exps for our simple measurement\n" ) ;
+    return FAILURE ;
+  }
   
   double *d = malloc( Nders * sizeof( double ) ) ;
-  double *fac = malloc( Nders * sizeof( double ) ) ;
   double Masses[ Nexps ] , Amps[ Nexps ] ;
-  const double p0 = 1.0 ;
-  
   size_t i ;
-  fac[0] = 1 ;
-  for( i = 1 ; i < Nders ; i++ ) {
-    fac[i] = fac[i-1] * i ;
-  }
 
   // compute the derivatives of the amplitudes
-  dLdp( d , fac , Nders , x , y , Ndata , p0 ) ;
+  dLdp( d , Nders , x , y , Ndata , p0 ) ;
   
   // get the poles of the pade representation
-  int Flag ;
-  Flag = stable_pade( Masses , d , Nexps , p0 ) ;
+  int Flag = SUCCESS ;
+  size_t Trials = Nexps , Nbest = 0 ;
+  for( Trials = Nexps ; Trials < Nexps + 8 ; Trials++ ) {
+    double Mtrials[ Trials ] ;
+    const size_t Ngood = stable_pade( Mtrials , d , Trials , p0 ) ;
+    if( Ngood >= Nexps ) {
+      size_t j ;
+      for( j = 0 ; j < Nexps ; j++ ) {
+	Masses[ j ] = Mtrials[ j ] ;
+      }
+      break ;
+    } else if( Ngood > Nbest ) {
+      Nbest = Ngood ;
+      size_t j ;
+      for( j = 0 ; j < Nexps ; j++ ) {
+	Masses[ j ] = Mtrials[ j ] ;
+      }
+    }
+  }
   if( Flag == FAILURE ) goto free ;
   
   // compute the amplitudes from the masses
@@ -242,7 +298,6 @@ pade_laplace( double *fparams ,
 
  free :
   free( d ) ;
-  free( fac ) ;
 
   return Flag ;
 }
